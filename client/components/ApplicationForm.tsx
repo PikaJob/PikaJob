0ef76850cc1adbec9e42f4b8b112ee7aa1ff7691
Nok-Nok <myngoc311@gmail.com>
2023-04-04 16:00:37 -0400

    Construct job tracker information

--- client/components/ApplicationForm.tsx
+++ client/components/ApplicationForm.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
-function ApplicationForm(): JSX.Element {
+import React, { ChangeEvent, MouseEvent } from 'react';
+type ApplicationFormProps = {
+  setUrl: React.Dispatch<React.SetStateAction<string>>;
+  applied: boolean;
+  setApplied: React.Dispatch<React.SetStateAction<boolean>>;
+  setSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function ApplicationForm(props: ApplicationFormProps): JSX.Element {
+  // ------------------- INTIALIZE STATE & EXTRACT PROPS -----------------------
+  const { setUrl, applied, setApplied, setSubmitted } = props;
+
+  // ------------------------- HANDLE CLICK METHODS ----------------------------
+
+  const handleUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newUrl: string = event.target.value;
+    setUrl(newUrl);
+  };
+
+  const handleAppliedChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newApplied: boolean = event.target.checked;
+    setApplied(newApplied);
+  };
+
+  const handleSubmit = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setSubmitted(true);
+  };
+
+  // ---------------------------- RENDER COMPONENT -----------------------------
   return (
     <form>
-      <input type='text' name='url' id='url' placeholder='url'></input>
-      <input type='checkbox' name='submitted' id='submitted'></input>
-      <label htmlFor='submitted'>Submitted</label>
-      <button></button>
+      <input type='text' name='url' id='url' placeholder='url' onChange={handleUrlChange}></input>
+      <input
+        type='checkbox'
+        name='applied'
+        id='applied'
+        onChange={handleAppliedChange}
+        checked={applied}
+      ></input>
+      <label htmlFor='applied'>Applied</label>
+      <button type='submit' onClick={handleSubmit}>
+        Submit
+      </button>
     </form>
   );
 }
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ApplicationForm from './ApplicationForm';
 import JobTracker from './JobTracker';
 
 function Home(): JSX.Element {
+  // create state for url (string)
+  const [url, setUrl] = useState('');
+  // create state for applied (boolean)
+  const [applied, setApplied] = useState(true);
+  // ^ need this for applicationForm, so to transfer that info to jobTracker
+  const [submitted, setSubmitted] = useState(false);
+
+  // get currently tracked jobs
+  //
+
   return (
     <>
-      <ApplicationForm />
-      <JobTracker />
+      <ApplicationForm
+        setUrl={setUrl}
+        applied={applied}
+        setApplied={setApplied}
+        setSubmitted={setSubmitted}
+      />
+      <JobTracker
+        url={url}
+        setUrl={setUrl}
+        applied={applied}
+        setApplied={setApplied}
+        submitted={submitted}
+        setSubmitted={setSubmitted}
+      />
     </>
   );
 }
